a6b427448779a654e796dc605309558d5d33a5a0
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-01-28 23:38:10 +0530

    Project Setup

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+
+
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.static("uploads"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
