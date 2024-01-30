6f4f35b22e5d4416943565be66787b1f27326a57
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-01-30 10:03:34 +0530

    Create and connect database and make model

---
+++ scripts/user.model.js
@@ -0,0 +1,24 @@
+import moongoose from 'mongoose';
+
+const userSchema = new moongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+
+    }
+}, { timestamps: true });
+
+const User = moongoose.model("User", userSchema);
+export default User;
